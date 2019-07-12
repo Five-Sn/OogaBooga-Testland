896a59403fa3fca26fc4eefa3f4a5f6fbc849a29
Five-Sn <32499540+Five-Sn@users.noreply.github.com>
2019-07-12 15:23:23 -0500

    Initial commit

---
+++ BestScript.py
@@ -0,0 +1,17 @@
+def main():
+    name = "main"
+    if name == "main":
+        print(does_name_equal_main(name))
+    else:
+        print("name probably == main")
+
+
+def does_name_equal_main(main):
+    if "name" == main:
+        return 'if __yowsers__ == "__mytrousers__"'
+    elif "name" == "maine":
+        return "ok"
+
+
+if __name__ == "__main__":
+    main()
