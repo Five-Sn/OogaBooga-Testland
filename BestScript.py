df08f948f80ce2125bcdb52c7182c95d95a302bb
Quintin Mitchell-Reyes <quintin.mitchell-reyes@q2ebanking.com>
2019-07-15 14:42:50 -0500

    suffering suffers

--- BestScript.py
+++ BestScript.py
@@ -18,6 +18,6 @@ def does_name_equal_main(main):
 
 if __name__ == "__main__":
     print("Hewlleow and wel wlcaome to ")
-    subprocess.call(["git", "rebase", "-i", "HEAD~2"])
+    # subprocess.call(["git", "rebase", "-i", "HEAD~2"])
     print("-GAhhhhhhhhsSA")
     # main()
