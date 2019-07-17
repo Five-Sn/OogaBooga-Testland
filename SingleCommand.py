1a072934a755a3d822f8f06e528506c9894d245e
Quintin Mitchell-Reyes <quintin.mitchell-reyes@q2ebanking.com>
2019-07-17 12:57:16 -0500

    unstaged changes

---
+++ SingleCommand.py
@@ -0,0 +1,41 @@
+import sys
+from subprocess import Popen, STDOUT, PIPE
+
+
+def git(*arguments):
+    print(arguments)
+    cmdline = ['git']
+    cmdline.extend(arguments)
+
+    # print('!GIT COMMAND!')
+    print(' '.join(arg for arg in cmdline))
+
+    proc = Popen(cmdline, stdout=PIPE, stderr=STDOUT)
+    print('')
+    output = []
+    while True:
+        line = proc.stdout.readline()
+        if not line:
+            break
+
+        line = line.rstrip().decode('utf-8')
+        print('git> {0}'.format(line))
+        output.append(line)
+
+    print()
+    return_code = proc.wait()
+    if return_code != 0:
+        print()
+        print('Git command terminated with exit code {0}.'.format(return_code))
+        sys.exit(1)
+
+    # print('!GIT COMMAND!')
+    return output
+
+
+def main():
+    git('rebase', '-s', '--onto', 'd16cc^', 'd16cc')
+
+
+if __name__ == "__main__":
+    main()
